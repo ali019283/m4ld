d7d21fc9c08bfda9f5aab4af38be79b44394a30d
Ali Burak <72763348+ali019283@users.noreply.github.com>
2022-07-18 15:17:11 +0300

    ok

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mald',
+    name='m4ld',
     version='0.1',
     license='MIT',
     author="Ali Burak Çetin",
     author_email='aliburakcetin@protonmail.com',
-    packages=find_packages('mald'),
-    package_dir={'': 'mald'},
-    url='https://github.com/ali019283/mald',
-    keywords='mald',
+    packages=find_packages('m4ld'),
+    package_dir={'': 'm4ld'},
+    url='https://github.com/ali019283/m4ld',
+    keywords='m4ld',
     install_requires=[],
 )
